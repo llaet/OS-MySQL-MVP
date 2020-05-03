8ed4eb70b1b0c61f7255408824845ff31213e777
Lucas Laet <50890960+llaet@users.noreply.github.com>
2020-05-03 17:36:26 -0300

    Overriding unnecessary information

--- dbinfox.sql
+++ dbinfox.sql
@@ -12,14 +12,11 @@ userPhone varchar(14),
 -- unique prevents same logins datas
 userLogin varchar(15) not null unique,
 userPassword varchar(15) not null,
-userProfile varchar(10) not null
+userProfile varchar(20) not null
 );
 
 insert into tbUsers(userID,userName,userPhone,userLogin,userPassword,userProfile)
 values(1,'Richard Hendricks','9999998-9979','admin','admin','admin');
-
-insert into tbUsers(userID,userName,userPhone,userLogin,userPassword,userProfile)
-values(2,'Lucas Lira','31999909-7799','lucas','123456','user');
 
 create table tbCustomers(
 customerID int primary key auto_increment,
@@ -32,6 +29,8 @@ customerMail varchar(50)
 create table tbSO(
 so int primary key auto_increment,
 timeSO timestamp default current_timestamp,
+serviceType varchar(15) not null,
+statusSO varchar(25) not null,
 product varchar(150) not null,
 productProblem varchar(150) not null,
 service varchar(150),
@@ -56,32 +55,9 @@ inner join tbCustomers as c
 -- assign 'idClient' of o as the same 'idClient' of c
 on (o.customerID = c.customerID);
 
--- append a new field on tbusers table
--- alter table tbusers add column userProfile varchar(20) not null;
-
--- delete the comlumn clientCPF on tbClients table
--- alter table tbclients drop clientCPF;
-
 -- search by a key word the content of table
 -- select * from tbClients where clientName like 'l%';
 
--- select * from tbUsers where idUser = 1 or customer = 'admin';
-
-alter table tbSO add serviceType varchar(15) not null after timeSO;
-alter table tbSO add statusSO varchar(25) not null after serviceType;
-
-insert into tbCustomers(customerName,customerAddress,customerPhone,customerMail)
-values('Teste da Silva','teste','99999-9999','teste@teste.com');
-
-insert into tbSO(serviceType,statusSO,product,productProblem,service,technician,price,customerID)
-values('Teste 01','Withdrawal ready','teste','problem','fix it','Lucas',99.90,1);
-
-delete from tbCustomers where customerID = 1;
-
 select * from tbUsers;
-
-describe tbso;
-describe tbusers;
+select * from tbCustomers;
 select * from tbSO;
-
-select technician from tbSO;
